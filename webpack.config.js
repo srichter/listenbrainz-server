9d083b5c7d67c6f439c950b569363d6a4cd03d88
Kartik Ohri <kartikohri13@gmail.com>
2021-09-20 23:10:53 +0530

    Fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = function (env, argv) {
         jsDir,
         "src/recommendations/Recommendations.tsx"
       ),
-      siteReports: path.resolve(jsDir, "src/stats/SiteReports.tsx"),
+      siteReports: path.resolve(jsDir, "src/stats/SitewideReports.tsx"),
     },
     output: {
       filename: isProd ? "[name].[contenthash].js" : "[name].js",
